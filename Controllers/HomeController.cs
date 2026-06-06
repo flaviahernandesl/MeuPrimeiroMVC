fe5806f478bb3c350e61bd864b48ce832e780581
Flavia <flaviahernandess@gmail.com>
2026-06-06 17:25:47 -0300

    Adicionada pagina do aluno e navegacao

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,6 +11,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Sobre()
+    {
+        return View();
+    }
+
     public IActionResult Privacy()
     {
         return View();
--- obj/Debug/net10.0/MeuPrimeiroMVC.AssemblyInfo.cs
+++ obj/Debug/net10.0/MeuPrimeiroMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeuPrimeiroMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a531bad729c1dc25cb29017be66f117549898cdf")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeuPrimeiroMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeuPrimeiroMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
